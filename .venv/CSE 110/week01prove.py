dc4ab682f2053ec0116d5ea7633d9a673ecca36f
cwilson397 <jedicolby16@gmail.com>
2024-04-24 10:55:50 -0600

    test thing

--- .venv/CSE 110/week01prove.py
+++ .venv/CSE 110/week01prove.py
@@ -1,18 +1,24 @@
+# Programmer Name: Colby Wilson
+# week 01 prove: Favorite Color Hex code
+# date: 4/24/2024
 # defining main function
+
 def main():
-    #defines the color choices and hex code lists
+    # defines the color choices and hex code lists
     colors = ['red', 'yellow', 'blue', 'green', 'orange', 'purple', 'pink', 'brown', 'white', 'gray', 'cyan', 'magenta']
     hex_codes = ['#FF0000', '#FFFF00', '#0000FF', '#008000', '#FFA500', '#800080', '#FFC0CB', '#A52A2A', '#FFFFFF', '#808080', '#00FFFF', '#FF00FF']
 
-    #instances a while true loop, so that if an invalid number is added
+    # instances a while true loop, so that if an invalid number is entered it will start the loop over
     while True:
+        # outputs the favorite color list
         print("Please select your favorite color out of the following list: ")
         for i, color in enumerate(colors, start=1):
             print(f"{i}. {color}")
 
+        # accepts the user's choice
         choice = int(input("Enter the number corresponding to your favorite color: "))
     
-
+        # takes the choice and outputs the part of the arrays that are corresponding to the choice, will start the loop over if invalid choice is entered
         if 1 <= choice <= len(colors):
             favorite_color = colors[choice - 1]
             hex_code = hex_codes[choice - 1]
@@ -21,5 +27,5 @@ def main():
         else:
             print("Invalid choice. Please select a number within the range.")
 
-if __name__ == "__main__":
-    main()
+# calls the main function
+main()
